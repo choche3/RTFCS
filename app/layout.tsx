0f2c99184bb4ce326bb14946376763a48e222b8a
Joseph Mwale <mwalejoseph138@gmail.com>
2026-03-10 11:19:43 +0200

    improving insight speed by making few adjustments in the layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,11 +8,13 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  display: "swap",
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -29,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className="bg-navy text-white"
+        className={`${geistSans.variable} ${geistMono.variable} bg-navy text-white`}
       >
         <Navbar />
         <main>{children}</main>
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
-import Image from "next/image";
+
 import Hero from "./components/sections/Hero";
 import ServiceSection from "./components/sections/ServicesSection";
 import ProjectsPreview from "./components/sections/ProjectsPreview";
 import CallToAction from "./components/sections/CallToAction";
+
+export const dynamic = "force-static"
+
 
 export default function Home() {
   return (
